9b7b7c91d68073b0c3a45636d8f8389da3210221
Mirzamehdi <mirzemehdi@gmail.com>
2022-03-28 21:40:34 +0200

    Implementing test and comparator class

--- src/main/java/Comparator.java
+++ src/main/java/Comparator.java
@@ -1,2 +1,7 @@
 public class Comparator {
+
+    public int getMaxValue(int a, int b) {
+        if (a > b) return a;
+        return b;
+    }
 }
--- src/test/java/ComparatorTest.java
+++ src/test/java/ComparatorTest.java
@@ -1,4 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ComparatorTest {
     private Comparator comparator;
+
+    @Before
+    public void setUp() {
+        comparator = new Comparator();
+    }
+
+    @Test
+    public void getMaxNumberTests() {
+        assertEquals("Max value between 20 and 30", 30, comparator.getMaxValue(20, 30));
+        assertEquals("Max value between 30 and 20", 30, comparator.getMaxValue(30, 20));
+        assertEquals("Max value between 30 and 30", 30, comparator.getMaxValue(30, 30));
+
+    }
+
+
 }
